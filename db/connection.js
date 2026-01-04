01d5c8275b298f09438645e50124c2527f320218
sunn02 <sollopez0002@hotmail.com>
2026-01-04 14:01:04 -0300

    feat: setup sequelize

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize({
+    dialect: 'postgres',
+    username: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT || 5432,
+
+});
+
+export default sequelize;
