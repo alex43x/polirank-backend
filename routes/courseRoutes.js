9bb9f1726a73933495fda71c37c2ccde976879f4
sunn02 <sollopez0002@hotmail.com>
2026-01-13 22:55:43 -0300

    feat: endpoints para estadisticas agregado

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -10,6 +10,8 @@ import {
   getReviewOfCourse,
   updateReviewOfCourse,
   deleteReviewOfCourse,
+  getReviewStats,
+  lastReviewStats
 } from "../controllers/reviewController.js";
 
 const courseRoutes = express.Router();
@@ -20,10 +22,13 @@ courseRoutes.get("/:id", getCourseById);
 courseRoutes.post("/", createCourse);
 
 // Rutas para reviews de cursos específicos
-courseRoutes.get("/:id/reviews/", getReviewsByCourse);
-courseRoutes.post("/:id/reviews/", createReviewForCourse);
+courseRoutes.get("/:id/reviews", getReviewsByCourse);
+courseRoutes.post("/:id/reviews", createReviewForCourse);
+courseRoutes.get("/:id/reviews/last", lastReviewStats);
+courseRoutes.get("/:id/reviews/history", getReviewStats);
 courseRoutes.get("/:id/reviews/:reviewId", getReviewOfCourse);
 courseRoutes.put("/:id/reviews/:reviewId", updateReviewOfCourse);
 courseRoutes.delete("/:id/reviews/:reviewId", deleteReviewOfCourse);
+
 
 export default courseRoutes;
